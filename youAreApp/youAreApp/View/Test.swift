24d27d4706b9f72a0a0ffb38f018ec3a8c92c3a6
Fabrício <guilhermo.fabricio@gmail.com>
2020-04-17 19:36:17 -0300

    Remove useless files

---
+++ youAreApp/youAreApp/View/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  youAreApp
+//
+//  Created by Fabrício Guilhermo on 17/04/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import SwiftUI
+
+extension ContentView {
+    Circle()
+    
+}
